3f2e343acb19c767314adfd76ffee5512bbc7cc5
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-05-05 21:35:05 -0700

    Finished sign in functionality on backend

--- server/server.js
+++ server/server.js
@@ -1,44 +1,79 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser'); 
+const formidable = require('express-formidable');
+const cloudinary = require('cloudinary');
 
 const app = express();
 const mongoose = require('mongoose');
+const async = require('async');
 require('dotenv').config();
 
+mongoose.set('useCreateIndex', true)
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
-
-app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const port = process.env.PORT || 3002;
+app.use(express.static('client/build'))
 
-app.listen(port, () => {
-  console.log('server running', port);
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+})
+
+// Models
+const { User } = require('./models/user');
+
+
+
+
+//=================================
+//              USERS
+//=================================
+
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body);
+
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success: true
+        })
+    })
 });
 
-///models
-const { User } = require('./models/user')
+app.post('/api/users/login',(req,res)=>{
+    User.findOne({'email':req.body.email},(err,user)=>{
+        if(!user) return res.json({loginSuccess:false,message:'Auth failed, email not found'});
 
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false,message:'Wrong password'});
 
-
-//----------------------------------
-//----------------------------------
-//              USERS
-//----------------------------------
-//----------------------------------
-
-app.post('/api/users/register', (req, res) => {
-    const user = new User(req.body)
-
-    user.save((err, doc) => {
-        if (err) return res.json({ success: false, err })
-        res.status(200).json({
-            success: true,
-            userData: doc
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth',user.token).status(200).json({
+                    loginSuccess: true
+                })
+            })
         })
     })
 })
 
+
+
+// DEFAULT 
+if( process.env.NODE_ENV === 'production' ){
+    const path = require('path');
+    app.get('/*',(req,res) => {
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
+
+
+const port = process.env.PORT || 3002;
+app.listen(port,()=>{
+    console.log(`Server Running at ${port}`)
+})

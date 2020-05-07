0deb9cb5d75c2858e5c97134157709f30e28f1d5
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-05-07 09:40:17 -0700

    built the wood model and routes

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const { admin } = require('./middleware/admin');
 // Models
 const { User } = require('./models/user');
 const { Brand } = require('./models/brand');
+const { Wood } = require('./models/wood');
 
 
 
@@ -37,6 +38,30 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 })
 
+
+
+//=================================
+//              WOODS
+//=================================
+
+app.post('/api/product/wood', auth, admin, (req, res) => {
+    const wood = new Wood(req.body);
+
+    wood.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        res.status(200).json({
+            success: true,
+            wood: doc
+        })
+    })
+});
+
+app.get('/api/product/woods', (req, res) => {
+    Wood.find({}, (err, woods) => {
+        if (err) return res.status(400).send(err);
+        res.status(200).send(woods)
+    })
+})
 
 
 

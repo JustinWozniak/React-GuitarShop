324461921cd958204b113ce45804f31e7065ec4e
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-05-07 14:32:12 -0700

    started working on app routes, a header and footer

---
+++ client/src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+import Layout from './hoc/layout'
+import Home from './components/Home'
+const Routes = () => {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Layout>
+  )
+}
+export default Routes;

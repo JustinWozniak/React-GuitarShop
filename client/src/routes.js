f60528e88abfbf141ec5087f9d982194a3f8c96d
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-05-08 08:35:05 -0700

    Working on signup/login...created button util

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import Layout from './hoc/layout'
 import Home from './components/Home/Home.Component'
+import RegisterLogin from './components/Register_Login'
+
+
+
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path="/register_login" exact component={RegisterLogin} />
         <Route path="/" exact component={Home} />
       </Switch>
     </Layout>

9f84e936a1d6a78391c014b4763d48d8837e471e
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-05-13 07:51:05 -0700

    preventing routes-- redirection depending on if user is authenticated or not

--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -1,13 +1,19 @@
 import {
     LOGIN_USER,
-    REGISTER_USER
+    REGISTER_USER,
+    AUTH_USER
 } from '../actions/types';
-export default function(state={},action){
-    switch(action.type){
+
+
+
+export default function (state = {}, action) {
+    switch (action.type) {
         case REGISTER_USER:
-            return {...state, register: action.payload }
+            return { ...state, register: action.payload }
         case LOGIN_USER:
             return { ...state, loginSucces: action.payload }
+        case AUTH_USER:
+            return { ...state, userData: action.payload }
         default:
             return state;
     }
--- client/src/routes.js
+++ client/src/routes.js
@@ -5,18 +5,18 @@ import Home from './components/Home/Home.Component'
 import RegisterLogin from './components/Register_Login/Register_Login.Component'
 import Register from './components/Register_Login/Register.Component'
 import UserDashboard from './components/User/User.Component'
-
+import Auth from './hoc/auth'
 
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
-      <Route path="/user/dashboard" exact component={UserDashboard} />
+        <Route path="/user/dashboard" exact component={Auth(UserDashboard, true)} />
 
-        <Route path="/register_login" exact component={RegisterLogin} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/" exact component={Home} />
+        <Route path="/register_login" exact component={Auth(RegisterLogin, false)} />
+        <Route path="/register" exact component={Auth(Register, false)} />
+        <Route path="/" exact component={Auth(Home, null)} />
       </Switch>
     </Layout>
   )

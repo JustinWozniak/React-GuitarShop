a296468b3ab1f5a3cb18afe40f86e33960615a1a
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-05-20 12:45:51 -0700

    Built user history

--- client/src/components/User/User.Component.js
+++ client/src/components/User/User.Component.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import UserLayout from '../../hoc/user';
 import MyButton from '../utils/button';
-
+import UserHistoryBlock from '../utils/User/history_block';
 
 const UserDashboard = ({ user }) => {
     return (
         <UserLayout>
             <div>
+
                 <div className="user_nfo_panel">
                     <h1>User information</h1>
                     <div>
@@ -20,19 +21,25 @@ const UserDashboard = ({ user }) => {
                         linkTo="/user/user_profile"
                     />
                 </div>
+
                 {
                     user.userData.history ?
                         <div className="user_nfo_panel">
                             <h1>History purchases</h1>
                             <div className="user_product_block_wrapper">
-                               
+                                <UserHistoryBlock
+                                    products={user.userData.history}
+                                />
                             </div>
                         </div>
 
                         : null
                 }
+
+
             </div>
         </UserLayout>
+
     );
 };
 

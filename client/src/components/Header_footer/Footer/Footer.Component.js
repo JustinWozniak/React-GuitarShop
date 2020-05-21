e584eda2fb47c8e30d9d8e42a855234a70ea7b2a
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-05-21 11:15:45 -0700

    changed apps name

--- client/src/components/Header_footer/Footer/Footer.Component.js
+++ client/src/components/Header_footer/Footer/Footer.Component.js
@@ -13,7 +13,7 @@ const Footer = ({data}) => {
         <footer className="bck_b_dark">
             <div className="container">
                 <div className="logo">
-                    Waves
+                  Craze Guitars
                 </div>
                 <div className="wrapper">
                     <div className="left">

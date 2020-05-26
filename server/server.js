51d578fa80a6ca83f15a3ea70417649c0514a314
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-05-26 10:31:57 -0700

    Now sending an email with purchase

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const mongoose = require('mongoose');
 const async = require('async');
 require('dotenv').config();
 
+const SHA1 = require("crypto-js/sha1");
+
 //middlewares
 const { auth } = require('./middleware/auth');
 const { admin } = require('./middleware/admin');
@@ -218,7 +220,7 @@ app.post('/api/users/register', (req, res) => {
     user.save((err, doc) => {
         if (err) return res.json({ success: false, err });
         sendEmail(doc.email, doc.name, null, "welcome")
-       return res.status(200).json({
+        return res.status(200).json({
             success: true
         })
     })
@@ -354,10 +356,13 @@ app.get('/api/users/removeFromCart', auth, (req, res) => {
 app.post('/api/users/successBuy', auth, (req, res) => {
     let history = [];
     let transactionData = {}
+    const date = new Date();
+    const po = `PO-${date.getSeconds()}${date.getMilliseconds()}-${SHA1(req.user._id).toString().substring(0, 8)}`
 
     // user history
     req.body.cartDetail.forEach((item) => {
         history.push({
+            porder: po,
             dateOfPurchase: Date.now(),
             name: item.name,
             brand: item.brand.name,
@@ -375,7 +380,10 @@ app.post('/api/users/successBuy', auth, (req, res) => {
         lastname: req.user.lastname,
         email: req.user.email
     }
-    transactionData.data = req.body.paymentData;
+    transactionData.data = {
+        ...req.body.paymentData,
+        porder: po
+    };
     transactionData.product = history;
 
     User.findOneAndUpdate(
@@ -405,7 +413,8 @@ app.post('/api/users/successBuy', auth, (req, res) => {
                         callback
                     )
                 }, (err) => {
-                    if (err) return res.json({ success: false, err })
+                    if (err) return res.json({ success: false, err });
+                    sendEmail(user.email, user.name, null, "purchase", transactionData)
                     res.status(200).json({
                         success: true,
                         cart: user.cart,
